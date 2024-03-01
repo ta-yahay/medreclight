3ec4f5decc53712570ea81bfd0da973a12f5b5a4
yahyakhalaf99 <yahya.ahmed99@eng-st.cu.edu.eg>
2024-03-01 16:58:16 +0200

    text

--- medrec/settings.py
+++ medrec/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['medrecgp.herokuapp.com']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
