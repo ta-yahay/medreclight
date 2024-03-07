4f7dc5a28ee348e21972bb6c4ef3048264a0fd3b
yahyakhalaf99 <yahya.ahmed99@eng-st.cu.edu.eg>
2024-03-07 16:34:20 +0200

    static

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medrec.settings')
+    settings_module='medrec.azuredep' if 'WEBSITE_HOSTNAME' in os.environ else 'medrec.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medrec.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- medrec/wsgi.py
+++ medrec/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+
 settings_module='medrec.azuredep' if 'WEBSITE_HOSTNAME' in os.environ else 'medrec.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
