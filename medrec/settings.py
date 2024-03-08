f2b11780d37040122c81969b4089b839ba2d8bad
yahyakhalaf99 <yahya.ahmed99@eng-st.cu.edu.eg>
2024-03-08 17:16:51 +0200

    staticedit

--- medrec/settings.py
+++ medrec/settings.py
@@ -26,13 +26,13 @@ SECRET_KEY = 'django-insecure-pbin@9c@xaie3!956(dw5(gfu3-95n6d90nq4$xb==3e)'
 # CSRF_TRUSTED_ORIGINS=['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
 DEBUG = True
-ALLOWED_HOSTS =['http://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# ALLOWED_HOSTS =['http://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['http://'+ os.environ['WEBSITE_HOSTNAME']]if 'WEBSITE_HOSTNAME' in os.environ else []
 
-# ALLOWED_HOSTS = ['http://lab3ta.azurewebsites.net/',
+ALLOWED_HOSTS = ['http://lab3ta.azurewebsites.net/','127.0.0.1']
 #  'http://lab3ta.azurewebsites.net',
 #  'lab3ta.azurewebsites.net/',
-#  'lab3ta.azurewebsites.net','127.0.0.1']
+#  'lab3ta.azurewebsites.net'
 # CSRF_TRUSTED_ORIGINS = ['http://lab3ta.azurewebsites.net/',
 #  'http://lab3ta.azurewebsites.net',
 #  'lab3ta.azurewebsites.net/',
