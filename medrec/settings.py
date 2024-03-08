80a931307593cfcc78be3ceeb40b33409df63af8
yahyakhalaf99 <yahya.ahmed99@eng-st.cu.edu.eg>
2024-03-08 16:11:40 +0200

    staticedit

--- medrec/settings.py
+++ medrec/settings.py
@@ -26,15 +26,15 @@ SECRET_KEY = 'django-insecure-pbin@9c@xaie3!956(dw5(gfu3-95n6d90nq4$xb==3e)'
 # CSRF_TRUSTED_ORIGINS=['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
 DEBUG = True
-
-ALLOWED_HOSTS = ['http://lab3ta.azurewebsites.net/',
- 'http://lab3ta.azurewebsites.net',
- 'lab3ta.azurewebsites.net/',
- 'lab3ta.azurewebsites.net','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://lab3ta.azurewebsites.net/',
- 'http://lab3ta.azurewebsites.net',
- 'lab3ta.azurewebsites.net/',
- 'lab3ta.azurewebsites.net','127.0.0.1']
+ALLOWED_HOSTS =[os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# ALLOWED_HOSTS = ['http://lab3ta.azurewebsites.net/',
+#  'http://lab3ta.azurewebsites.net',
+#  'lab3ta.azurewebsites.net/',
+#  'lab3ta.azurewebsites.net','127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['http://lab3ta.azurewebsites.net/',
+#  'http://lab3ta.azurewebsites.net',
+#  'lab3ta.azurewebsites.net/',
+#  'lab3ta.azurewebsites.net','127.0.0.1']
 
 
 # ALLOWED_HOSTS =[os.environ['WEBSITE_HOSTNAME']]
